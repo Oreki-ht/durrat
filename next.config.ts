a164447aa77c0581c19cfb2aaf20b924e5913976
oreki <condou23@gmail.com>
2025-10-01 18:52:39 +0300

    removed pm2

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     domains: ['images.unsplash.com', 'randomuser.me'],
+    unoptimized: true,
   },
+
+  distDir: 'out'
 }
 
 module.exports = nextConfig
